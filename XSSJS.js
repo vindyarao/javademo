3242d8e1112bf98802022cd83aa6d540a8a8132d
vindyarao <48816025+vindyarao@users.noreply.github.com>
2020-08-01 18:49:01 +0530

    Add files via upload

---
+++ XSSJS.js
@@ -0,0 +1,97 @@
+// var ModalPopUp = '<%=ModalRequestDetails.ClientID %>'; -pfp
+'Response.Write("<br>")
+'Response.Write("---")"
+<!--Use this dependency to store the SAP Logon Tickets in the HTTP session (instead of in ElastiCache manually):
+@*<input type="button" title="Search" class="btn btn-default" value="Search" onclick="location.href='@Url.Action("List", "Skills")'" />*@  --pfp
+//document.write('</label>	')
+
+<td class="tableheader-gray" onclick="searchOpen(Document.href.url);" style="cursor: hand"> -Valid
+
+document.write("variable ");
+document.write(' ')
+document.write('<!-- skills -->')
+
+document.write( "<td class='scwWeek' id='scwWeekInit" + i + "'></td>");  -Valid
+document.write('<script type="text/javascript" src="'+libraryName+'"></script>');  -Valid
+
+el.innerHTML = "<a href='#'></a>";
+body.innerHTML = "<form></form><form></form>";
+
+
+container.innerHTML = val.html;   - valid
+div.innerHTML = '<table><tbody>' + this.content + '</tbody></table>';   - valid
+divRequests.InnerHtml = htmlContents.ToString();  - valid
+OnClick="btnSave_Click"></asp:Button>
+OnClick="BtnSearchRequest_Click" />
+Visible="false" OnClick=BtnPushDate_Click/> --- valid
+
+
+<td class="tableheader-gray" onclick="searchOpen(Document.href.url);" style="cursor: hand">  - valid
+
+Public Property UserRule_Session() As IList(Of UserRuleDTO) Implements IAdministerSecurityView.UserRule_Session
+Public Sub ClearSession()
+setUserSession(principal.PeopleKey)
+perfyear.TypeID = Int32.Parse(Request.QueryString("Type"))
+
+if (Request.QueryString["stacktrc"] != null)
+	
+Private Sub RevId_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles RevId.TextChanged
+txtPrevL4Rel.Text = "";
+using System.Text;
+
+txtPersonnelID= sPeronnelID.text;         - valid
+
+ if (txtStartMth.Text == "") 
+If String.IsNullOrEmpty(txtAnswerRemarksSUP.Text) Then
+else if (cboSource.Text.ToUpper().Equals("PROD"))
+	
+intOUID = lblOUID == null ? 0 : Convert.ToInt32(lblOUID.Text.Trim()); 
+ chdChldDirectLock.Enabled = (Convert.ToBoolean(lblEnabledNo.Text) && Convert.ToBoolean(hdnIsBayDirectLocking.Value)); 
+sumGDN -= Convert.ToDouble(box2.Text); 
+sumGDN += Convert.ToDecimal(box2.Text); 
+
+$window.location.href = '/#';
+
+Response.Write("Session End. Please open application in different session.");
+
+Response.Write(gridData);         - valid
+Response.Write("Session End. Please open application in different session",xvx");  - valid
+
+
+perfyear.TypeID = Int32.Parse(Request.QueryString("Type"))
+----------------------------------------------------------------------------
+
+/* SCenarios from CIO */
+
+@Value("$&oflwebwolf.url&cfl") 
+ @Value("$&oflwebwolf.url.mail&cfl") 
+ @Value("$&oflwebgoat.server.directory&cfl") 
+
+$window.location.href = abc   ---valid
+$window.location.href = '/#';  --FP
+'Response.Write("---")  --FP
+'Response.Write("<br>")   --FP
+<!--Use this dependency to store the SAP Logon Tickets in the HTTP session (instead of in ElastiCache manually):      --FP
+//document.write('</label>	')  --FP
+
+document.write("variable+abc");  --FP
+document.write(' ')           --FP
+document.write('<!-- skills -->')       --FP
+document.write(abc)     --valid
+document.write("<td class='scwWeek' id='scwWeekInit"" + i + ""'></td>");       --- Valid
+document.write('<script type=""text/javascript"" src=""'+libraryName+'""></script>');     -- Valid
+
+
+el.innerHTML = "<a href='#'></a>";      --FP
+body.innerHTML = "<form></form><form></form>";        --FP
+//container.innerHTML = val.html;   --Valid
+//div.innerHTML = '<table><tbody>' + this.content + '</tbody></table>';   --valid
+container.innerHTML = val.html;   --Valid
+div.innerHTML = '<table><tbody>' + this.content + '</tbody></table>';   --valid
+divRequests.InnerHtml = htmlContents.ToString();"  --valid
+OnClick="btnSave_Click"></asp:Button>     --FP
+OnClick="BtnSearchRequest_Click" />   --FP
+Visible="false" OnClick="BtnPushDate_Click" />        --FP
+Public Property UserRule_Session() As IList(Of UserRuleDTO) Implements IAdministerSecurityView.UserRule_Session            --is not XSS
+Public Sub ClearSession()           -- is not XSS
+setUserSession(principal.PeopleKey)   ---is not XSS

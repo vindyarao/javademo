a3214f9a17e7bb69394b26315da24140aefd5035
vindyarao <48816025+vindyarao@users.noreply.github.com>
2020-08-01 18:33:54 +0530

    Add files via upload

---
+++ CIOXSSData.java
@@ -0,0 +1,7 @@
+//  var ModalPopUp = '<%=ModalRequestDetails.ClientID %>';
+'Response.Write(""""""""---"""""""")
+'Response.Write(""""""""<br>"""""""")""
+<!--Use this dependency to store the SAP Logon Tickets in the HTTP session (instead of in ElastiCache manually):
+@*<input type=""button"" title=""Search"" class=""btn btn-default"" value=""Search"" onclick=""location.href='@Url.Action(""List"", ""Skills"")'"" />*@
+//document.write('</label>	')
+"
